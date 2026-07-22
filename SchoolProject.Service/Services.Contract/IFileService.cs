0aac92b9604c3e9e49b3ab4db0dd5a29ceda60ef
Mohamed Bedir <20201700709@cis.asu.edu.eg>
2026-07-23 02:43:54 +0300

    Service Upload Image

---
+++ SchoolProject.Service/Services.Contract/IFileService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Service.Services.Contract
+{
+    public interface IFileService
+    {
+        Task<string> UploadImageAsync(IFormFile file,string folder);
+        //Task<string> DeleteImageAsync(string imagename,string folder);
+    }
+}
---
+++ SchoolProject.Service/Services/FileService.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using SchoolProject.Service.Services.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolProject.Service.Services
+{
+    public class FileServiceL:IFileService
+    {
+        private readonly IWebHostEnvironment hostEnvironment;
+
+        public FileServiceL(IWebHostEnvironment hostEnvironment)
+        {
+            this.hostEnvironment = hostEnvironment;
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile file, string folder)
+        {
+            if (file == null || file.Length == 0)
+                return "NoImage";
+
+            var extension = Path.GetExtension(file.FileName).ToLower();
+
+            var allowedExtensions = new[]
+            {
+                ".jpg",
+                ".jpeg",
+                ".png",
+                ".gif",
+                ".webp"
+             };
+
+            if (!allowedExtensions.Contains(extension))
+                return "InvalidImage";
+
+            if (file.Length > 5 * 1024 * 1024)
+                return "ImageTooLarge";
+
+            var imageName = $"{Guid.NewGuid()}{extension}";
+
+            var folderPath = Path.Combine(hostEnvironment.WebRootPath, folder);
+
+            if (!Directory.Exists(folderPath))
+                Directory.CreateDirectory(folderPath);
+
+            var filePath = Path.Combine(folderPath, imageName);
+
+            try
+            {
+                using var stream = new FileStream(filePath, FileMode.Create);
+
+                await file.CopyToAsync(stream);
+
+                return $"{folder}/{imageName}";
+            }
+            catch
+            {
+                return "FailedToUploadImage";
+            }
+        }
+    }
+}
